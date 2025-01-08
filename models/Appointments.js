a973d16a7ce79acc5886c3062450562ec0a393d2
adina8244 <38214959736@mby.co.il>
2025-01-08 15:59:03 +0200

    add appointment schema

---
+++ models/Appointments.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const AppointmentsSchema = new mongoose.Schema({
+    identity:{
+        type: String, 
+        require: true
+    }, 
+    baby_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        require: true , 
+        ref: "Babies"
+    },
+    nurse_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        require: true , 
+        ref: "User"
+    } ,
+    status: {
+        type: String,
+        enum: ['canceled','completed','confirmed','pending'],
+        default:false
+        },
+        appointment_time:{
+            time:{ type:Number ,require: true  },
+            date:{ type:Date ,require: true  }
+        }
+})
+
+module.exports= mongoose.model('Babies', AppointmentsSchema)
