22bbcbf1c8654f85267ee2b91d5a9bfc900faf65
שרה הלוי <328241377@mbydomain>
2025-01-08 15:34:40 +0200

    add nurse schedule

---
+++ models/NurseSchedule.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const NurseScheduleSchema = new mongoose.Schema({
+    nurse_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        require: true , 
+        ref: "Nurse"
+    }, 
+    working_day:{
+        type: Date, 
+        require: true
+    }, 
+    satrt_time:{
+        type: Number,
+        require: true 
+    }, 
+    end_time:{
+        type: Number,
+        require: true 
+    }
+})
+
+module.exports= mongoose.model('NurseSchedule', NurseScheduleSchema)
