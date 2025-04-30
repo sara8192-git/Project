eeed041af4c0eb524c29b4c0274991db9059c0c6
sara8192-git <38328241377@mby.co.il>
2025-04-30 18:44:08 +0300

    add picture in Nurse

--- client/my-app/src/Component/Nurse.jsx
+++ client/my-app/src/Component/Nurse.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { InputText } from "primereact/inputtext";
-import { Password } from "primereact/password";
-import { Button } from "primereact/button";
-import { useDispatch, useSelector } from 'react-redux';
-import { setToken, logOut } from '../redux/tokenSlice'
 import { Menubar } from 'primereact/menubar';
 import QueueSummaryNurse from './QueueSummaryNurse'
 import { Route, Routes } from 'react-router-dom'
 import ChatNurse from './ChatNurse'
 import TestsAndStatistics from "./TestsAndStatistics"
-export default function Nurse() {
+import defaultProfilePicture from "../WIN_20250430_18_06_45_Pro.jpg";
+import { useSelector } from "react-redux"; // לשימוש בפרטי המשתמש מ-Redux
 
+export default function Nurse() {
+    const name = useSelector((state) => state.token.user.name)
+    const user = useSelector((state) => state.token.user);
+    const profilePicture = user?.profilePicture
+    ? `http://localhost:7002${user.profilePicture}` // נתיב מוחלט לתמונה שהועלתה
+    : defaultProfilePicture; // תמונה דיפולטיבית
+    
     const navigate = useNavigate(); // 🔹 מאפשר ניווט לדפים אחרים
     const items = [
         {
@@ -35,18 +37,27 @@ export default function Nurse() {
             command: () => {
                 navigate('/nurse/chat');
             }},
-        // {
-        //     label: 'Projects',
-        //     icon: 'pi pi-search',
-        //     command: () => {
-        //         navigate('/nurse/BookedAppointmentParent')
-        //     }
-        // }
+      
     ];
-
+    const endTemplate = (
+        <div className="flex align-items-center">
+            <img
+                src={profilePicture} // נתיב התמונה
+                className="profile-picture"
+                style={{
+                    width: "40px",
+                    height: "40px",
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                    marginRight: "10px",
+                }}
+            />
+            <span>{name || "Guest"}</span>
+        </div>
+    );
     return (
         <div className="flex flex-column align-items-center">
-            <Menubar model={items} />
+            <Menubar model={items} end={endTemplate}/>
             <Routes>
                 <Route path='/QueueSummaryNurse' element={<QueueSummaryNurse />} />
                 <Route path='/ChatNurse' element={<ChatNurse />} />
--- client/my-app/src/Component/Secretary.jsx
+++ client/my-app/src/Component/Secretary.jsx
@@ -7,8 +7,15 @@ import ChatParent from './ChatParent'
 import {  Route, Routes } from 'react-router-dom'
 import AddBabySecretary from './AddBabySecretary'
 import AddScheduleNurse from './AddScheduleNurse'
+import defaultProfilePicture from "../WIN_20250430_18_06_45_Pro.jpg";
+import { useSelector } from "react-redux"; // לשימוש בפרטי המשתמש מ-Redux
 
 export default function Parent() {
+    const name = useSelector((state) => state.token.user.name)
+    const user = useSelector((state) => state.token.user);
+    const profilePicture = user?.profilePicture
+    ? `http://localhost:7002${user.profilePicture}` // נתיב מוחלט לתמונה שהועלתה
+    : defaultProfilePicture; // תמונה דיפולטיבית
 
     const navigate = useNavigate(); // 🔹 מאפשר ניווט לדפים אחרים
     const items = [
@@ -42,10 +49,25 @@ export default function Parent() {
           }
           
     ];
-
+const endTemplate = (
+        <div className="flex align-items-center">
+            <img
+                src={profilePicture} // נתיב התמונה
+                className="profile-picture"
+                style={{
+                    width: "40px",
+                    height: "40px",
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                    marginRight: "10px",
+                }}
+            />
+            <span>{name || "Guest"}</span>
+        </div>
+    );
     return (
         <div className="flex flex-column align-items-center">
-            <Menubar model={items} />
+            <Menubar model={items} end={endTemplate}/>
             <Routes>
                 <Route path='/BookedAppointmentParent' element={<BookedAppointmentParent />} />
                 <Route path='/UseCalendar' element={<UseCalendar />} />
