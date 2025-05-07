eb3f1bd126c5f37a4e7e7f42b255ca4856bc9272
sara8192-git <38328241377@mby.co.il>
2025-05-07 21:08:36 +0300

    pictures of our screans of the project

--- server/server/config/corsOptions.js
+++ server/server/config/corsOptions.js
@@ -1,16 +1,16 @@
+
+const allowedOrigins = [
+    'http://localhost:3000']
 const corsOptions = {
-    // origin: (origin, callback) => {
-    //     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-    //         callback(null, true);
-    //     } else {
-    //         console.log("לא מורשה לגישה ממקור: ", origin);  // הוספת פלט לטרייסינג של הבעיה
-    //         callback(new Error('Not allowed by CORS'));
-    //     }
-    // },
-    // credentials: true,
-    // optionsSuccessStatus: 200
-    origin: 'http://localhost:3000', // או כל דומיין אחר שאתה רוצה לאפשר
-    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-};
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        }
+        else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+module.exports = corsOptions
